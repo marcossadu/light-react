246d06975304397a16dd9724a11e015795b797e6
marcossadu <masadu@outlook.es>
2023-05-25 14:21:50 +0200

    bombillita

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import LightOnImg from './assets/lighton.png'
 import LightOffImg from './assets/lightoff.png'
+import {useState} from 'react' 
 
 function App() {
 
@@ -12,11 +13,20 @@ function App() {
 }
 
 function Light() {
+const [statusLight, setStatusLight]=useState("on")
 
+function HandleLightOn(){
+  setStatusLight("on")
+}
+function HandleLightOff(){
+  setStatusLight("off")
+}
   return <div>
     <h1>Click the buttons to turn on or off the light</h1>
-    <img src={LightOnImg} />
-    <div><button>Turn On</button><button>Turn Off</button></div>
+    <img src={statusLight === "on"?LightOnImg:LightOffImg} />
+    <div>
+      <button onClick={HandleLightOn} style={{display: statusLight === "on"? "none" :"inline"}}>Turn On</button>
+    <button onClick={HandleLightOff} style={{display:statusLight ==="on"? "inline":"none"}}>Turn Off</button></div>
   </div>
 }
 
